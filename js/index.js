89f684bddb9b01ef0b05422e0a37f6321658d482
IsraP <IsraP@users.noreply.github.com>
2020-05-20 20:20:31 -0300

    Adicionando tudo de uma vez

---
+++ js/index.js
@@ -0,0 +1,95 @@
+window.onload = () => {
+    Smooth_hrefs: {
+			$(function () {
+			$('a[href*="#"]:not([href="#"])').click(function () {
+
+				var target = $(this.hash);
+
+				if (target.length) {
+					$('html, body').animate({ scrollTop: target.offset().top }, 1000);
+					return false;
+				}
+
+			});
+		});
+    }
+
+    Botoes_Textos: {
+        megaimg.onclick = () =>{
+            let megaX = document.getElementById("megatext");
+            if (megaX.style.display === "none") {
+                megaX.style.display = "block";
+                window.location.href = "#megaimg";
+              } 
+              else {
+                megaX.style.display = "none";
+              }
+            
+            
+        }
+        ryuimg.onclick = () =>{
+            let ryu = document.getElementById("ryutext");
+            let divisor1 = document.getElementById("divisor1");
+            window.location.href = "#ryuimg";
+            if (ryu.style.display === "none") {
+                ryu.style.display = "block";
+                divisor1.style.display = "block";
+              } 
+              else {
+                ryu.style.display = "none";
+                divisor1.style.display = "none";
+              }
+            
+            
+        }
+        zeroimg.onclick = () =>{
+            let personagem = document.getElementById("zerotext");
+            let divisor = document.getElementById("divisor2");
+            window.location.href = "#zeroimg";
+            if (personagem.style.display === "none") {
+                personagem.style.display = "block";
+                divisor.style.display = "block";
+              } 
+            else {
+                personagem.style.display = "none";
+                divisor.style.display = "none";
+            }
+        }
+        kenimg.onclick = () =>{
+            let personagem = document.getElementById("kentext");
+            window.location.href = "#kenimg";
+            if (personagem.style.display === "none") {
+                personagem.style.display = "block";
+              } 
+            else {
+                personagem.style.display = "none";
+            }
+        }
+        turingimg.onclick = () =>{
+            let personagem = document.getElementById("turingtext");
+            let divisor = document.getElementById("divisor3");
+            window.location.href = "#turingimg";
+            if (personagem.style.display === "none") {
+                personagem.style.display = "block";
+                divisor.style.display = "block";
+              } 
+            else {
+                personagem.style.display = "none";
+                divisor.style.display = "none";
+            }
+        }
+        metalimg.onclick = () =>{
+            let personagem = document.getElementById("metaltext");
+            let divisor = document.getElementById("divisor4");
+            window.location.href = "#metalimg";
+            if (personagem.style.display === "none") {
+                personagem.style.display = "block";
+                divisor.style.display = "block";
+              } 
+            else {
+                personagem.style.display = "none";
+                divisor.style.display = "none";
+            }
+        }
+    }   
+}
